ec8a4483c8dc5bfeeec47f0398fb695b48fd288d
acandael <info@anthonycandaele.com>
2019-03-10 11:25:16 +0100

    implement main route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,68 @@
 import React, { Component } from 'react';
 import './App.css';
-import api from './config.js';
+import apiKey from './config.js';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import axios from 'axios';
+
+// Import components
+import Header from './components/Header';
+import Nav from './components/Nav';
+import SearchResults from './components/SearchResults';
+
+// Create a variable to store the API key
+const api = apiKey;
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      pics: [],
+      loading: true
+    };
+  }
+
+  componentDidMount() {
+    this.mainSearch();
+  }
+
+  mainSearch = (query = 'cats') => {
+    axios
+      .get(
+        `https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${api}&tags=${query}&sort=relevance&per_page=24&format=json&nojsoncallback=1`
+      )
+      .then(response => {
+        this.setState({
+          mainPics: response.data.photos.photo,
+          loading: false
+        });
+      })
+      .catch(function(error) {
+        console.log('Error fetching and parsing data', error);
+      });
+  };
+
   render() {
-    return <div className="App">Gallery App</div>;
+    return (
+      <BrowserRouter>
+        <div className="container">
+          <Header />
+          <Nav />
+          <Switch>
+            <Route
+              exact
+              path="/"
+              render={() =>
+                this.state.loading ? (
+                  <p> Loading...</p>
+                ) : (
+                  <SearchResults data={this.state.mainPics} />
+                )
+              }
+            />
+          </Switch>
+        </div>
+      </BrowserRouter>
+    );
   }
 }
 
---
+++ src/components/SearchResults.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import Picture from './Picture';
+import NoResults from './NoResults';
+
+const SearchResults = props => {
+  // {/* display seach results here
+  // if no search results available,
+  // display an error message */}
+  const results = props.data;
+  let pics;
+  if (results) {
+    pics = results.map(pic => (
+      <Picture
+        url={`https://farm${pic.farm}.staticflickr.com/${pic.server}/${
+          pic.id
+        }_${pic.secret}.jpg`}
+        key={pic.id}
+        title={pic.title}
+      />
+    ));
+  } else {
+    pics = <NoResults />;
+  }
+
+  return (
+    <div className="photo-container">
+      <h2>Search Results</h2>
+      <ul>{pics}</ul>
+    </div>
+  );
+};
+
+export default SearchResults;
